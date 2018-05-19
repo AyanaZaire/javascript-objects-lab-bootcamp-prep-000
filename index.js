349b2ec85394362a5a9fd56ba1dfc2b745bf10b9
AyanaZaire <ayanazaire@gmail.com>
2018-05-19 02:08:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,5 @@ function updateObjectWithKeyAndValue(object, key, value){
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return Object.assign(object, { [key]: value })
 }
+
+function 
